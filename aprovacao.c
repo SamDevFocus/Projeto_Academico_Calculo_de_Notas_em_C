aa66a539b683fbff322e86bbe0c825473314e2a3
vini006dev <vini.alves120587@gmail.com>
2025-04-07 20:36:20 -0300

    Add de switch case
    foi adicionado no código uma opção com switch case, para o usuário escolher qual nota da avaliação inserir primeiro.

--- aprovacao.c
+++ aprovacao.c
@@ -6,28 +6,68 @@ int main() {
     float avaliacaoFinal;
     float menorNota;
     float soma;
+    int escolha;
 
     printf("Ola, bem-vindo ao calculador oficial de aprovacao da Universidade Braz Cubas\n");
 
-    do{
-    	printf("Por favor, insira sua nota na Avaliacao UM:\n");
-    	scanf("%f", &avaliacaoUm);
-    
-    if (avaliacaoUm < 0 || avaliacaoUm > 5){
-    	printf("Nota invalida! Digite um valor entre 0 e 5\n");
-	}
-    
-	}while (avaliacaoUm < 0 || avaliacaoUm > 5);
-	
-	do {
-		printf("Por favor, insira sua nota na Avaliacao DOIS:\n");
-    	scanf("%f", &avaliacaoDois);
-    	
-    if (avaliacaoDois < 0 || avaliacaoDois > 5){
-    	printf("Nota invalida! Digite um valor entre 0 e 5\n");
-	}
-	
-	} while (avaliacaoDois < 0 || avaliacaoDois > 5);
+    // Loop até o usuário digitar uma opção válida (1 ou 2)
+    do {
+        printf("Qual avaliacao voce quer inserir primeiro? (1 - Avaliacao A1, 2 - Avaliacao A2): ");
+        scanf("%d", &escolha);
+
+        if (escolha != 1 && escolha != 2) {
+            printf("Opcao invalida! Tente novamente.\n");
+        }
+
+    } while (escolha != 1 && escolha != 2);
+
+
+    switch(escolha) {
+
+        case 1:
+            do {
+                printf("Por favor, insira sua nota na Avaliacao UM:\n");
+                scanf("%f", &avaliacaoUm);
+
+                if (avaliacaoUm < 0 || avaliacaoUm > 5) {
+                    printf("Nota invalida! Digite um valor entre 0 e 5\n");
+                }
+
+            } while (avaliacaoUm < 0 || avaliacaoUm > 5);
+
+            do {
+                printf("Por favor, insira sua nota na Avaliacao DOIS:\n");
+                scanf("%f", &avaliacaoDois);
+
+                if (avaliacaoDois < 0 || avaliacaoDois > 5) {
+                    printf("Nota invalida! Digite um valor entre 0 e 5\n");
+                }
+
+            } while (avaliacaoDois < 0 || avaliacaoDois > 5);
+            break;
+
+        case 2:
+            do {
+                printf("Por favor, insira sua nota na Avaliacao DOIS:\n");
+                scanf("%f", &avaliacaoDois);
+
+                if (avaliacaoDois < 0 || avaliacaoDois > 5) {
+                    printf("Nota invalida! Digite um valor entre 0 e 5\n");
+                }
+
+            } while (avaliacaoDois < 0 || avaliacaoDois > 5);
+
+            do {
+                printf("Por favor, insira sua nota na Avaliacao UM:\n");
+                scanf("%f", &avaliacaoUm);
+
+                if (avaliacaoUm < 0 || avaliacaoUm > 5) {
+                    printf("Nota invalida! Digite um valor entre 0 e 5\n");
+                }
+
+            } while (avaliacaoUm < 0 || avaliacaoUm > 5);
+            break;
+    }
 
     soma = avaliacaoUm + avaliacaoDois;
 
@@ -35,7 +75,7 @@ int main() {
         printf("Aprovado com nota final de %.2f\n", soma);
     } else {
         printf("Voce nao alcancou a nota minima. Sua nota foi %.2f\n", soma);
-        
+
         do {
             printf("Por favor, insira a nota da Avaliacao Final:\n");
             scanf("%f", &avaliacaoFinal);
@@ -60,7 +100,7 @@ int main() {
         soma = avaliacaoUm + avaliacaoDois;
 
         if (soma >= 6.0) {
-		printf("Aprovado com uso da avaliacao final. Nota final: %.2f\n", soma);
+            printf("Aprovado com uso da avaliacao final. Nota final: %.2f\n", soma);
         } else {
             printf("Reprovado com nota final de %.2f\n", soma);
         }
@@ -68,4 +108,3 @@ int main() {
 
     return 0;
 }
-
